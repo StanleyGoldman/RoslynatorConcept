2dc9729c300a8d8d803776d233d6050311cba10b
Stanley Goldman <Stanley.Goldman@gmail.com>
2018-06-29 20:04:42 -0400

    Add project files.

---
+++ RoslynatorConcept/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoslynatorConcept
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
